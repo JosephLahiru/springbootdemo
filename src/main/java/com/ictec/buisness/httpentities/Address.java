f8b9310d62dbdb20eb2b9fee3bdcc8107a7ea7b3
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-05-21 09:53:24 +0530

    added new Fields to Address

--- src/main/java/com/ictec/buisness/httpentities/Address.java
+++ src/main/java/com/ictec/buisness/httpentities/Address.java
@@ -5,6 +5,9 @@ public class Address {
     private String line2;
     private String state;
 
+    private String city;
+    private String country;
+
     public String getLine1() {
         return line1;
     }
@@ -27,5 +30,21 @@ public class Address {
 
     public void setState(String state) {
         this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
